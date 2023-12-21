7af1a2ed22ccd676c464c0197482d215df30fcef
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-12-21 09:11:40 -0500

    add logo

--- src/reveal.svelte
+++ src/reveal.svelte
@@ -46,7 +46,10 @@
     data-tauri-drag-region
     class="flex border-b rounded-t-lg w-full select-none px-4 py-2"
   >
-    <div class="flex gap-2">
+    <div class="flex items-center gap-2">
+      <div>
+        <img alt="" src="/icon.png" class="w-5 h-5 invert" />
+      </div>
       <div class="text-blue-500">reveal</div>
       {#await getVersion()}
         <div></div>
