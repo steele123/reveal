cdf2df4db70ce350830a3f09ffb3fcdf8d46650b
Steele Scott <60533996+steele123@users.noreply.github.com>
2023-12-21 09:07:21 -0500

    add transitions

--- src/reveal.svelte
+++ src/reveal.svelte
@@ -142,12 +142,12 @@
   </div>
   <div class="px-4 items-center flex pt-1 border-t">
     {#if connected}
-      <div class="flex items-center gap-1 text-sm">
+      <div in:fade={{ duration: 2000 }} class="flex items-center gap-1 text-sm">
         <div class="bg-green-500 animate-pulse h-2 w-2 rounded-full" />
         <div>Connected to League Client</div>
       </div>
     {:else}
-      <div class="flex items-center gap-1 text-sm">
+      <div in:fade={{ duration: 2000 }} class="flex items-center gap-1 text-sm">
         <div class="bg-red-500 animate-pulse h-2 w-2 rounded-full" />
         <div>Not Connected to League Client</div>
       </div>
