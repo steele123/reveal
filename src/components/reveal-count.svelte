b3006897f6ffc7d5ac21db74a05bdfb83da8bc78
Steele Scott <60533996+steele123@users.noreply.github.com>
2024-02-16 19:40:05 -0500

    dont use reveal eventstream anymore since its useless

--- src/components/reveal-count.svelte
+++ src/components/reveal-count.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { fetch } from "@tauri-apps/api/http";
   import { IS_DEV } from "../lib/constants";
 
   let revealCount = 0;
 
-  onMount(async () => {
-    const eventSource = new EventSource("https://api.hyperboost.gg/reveal/events");
-    eventSource.addEventListener("lobby_reveal", (event) => {
-      const count = event.data as number;
-      revealCount = count;
-    });
+  onMount(() => {
+    updateRevealCount();
+
+    const clear = setInterval(async () => {
+      await updateRevealCount();
+    }, 60000);
+
+    return () => {
+      clearInterval(clear);
+    };
   });
+
+  async function updateRevealCount() {
+    const resp = await fetch("https://hyperboost.gg/api/reveal/stats");
+
+    const data = resp.data as {
+      version: string;
+      revealedLobbies: number;
+      downloads: number;
+    };
+
+    revealCount = data.revealedLobbies;
+  }
 </script>
 
 {revealCount}
